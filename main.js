ae93479bde2df217a7c18746abb7830b6f66a5d6
DawiT <Dawid@Tararuj.be>
2021-01-27 09:25:28 +0100

    fin

---
+++ main.js
@@ -0,0 +1,36 @@
+//1 
+
+const redP = document.getElementsByClassName("redPurple");
+ 
+console.log(redP); 
+
+
+// 2 
+const redP2 = document.querySelectorAll(".redPurple");
+
+console.log(redP2);
+
+
+// 3 
+const redph1 = document.querySelectorAll("h1.redPurple");
+
+console.log(redph1); 
+
+
+// 4 
+
+const lipspan = document.querySelectorAll("li, p, span"); 
+
+console.log(lipspan); 
+
+// 5 
+
+const liimP = document.querySelectorAll("li.important, p");
+
+console.log(liimP); 
+
+// 6 
+
+const h1spanredP = document.querySelectorAll("h1.redPurple, span.redPurple");
+
+console.log(h1spanredP); 
